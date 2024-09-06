12b6ffa4ae415151a91d65f524da4567fe32b78d
Sasya-reddy <sasyareddy471@gmail.com>
2024-09-06 11:08:57 +0530

    cc

--- React/HandsOn/react-usereducer-hook/src/App.js
+++ React/HandsOn/react-usereducer-hook/src/App.js
@@ -4,12 +4,14 @@ import Demo1 from "./demo1";
 import Demo2 from "./demo2";
 import Demo3 from "./demo3";
 
+
 function App() {
   return (
     <div className="App">
       {/* <Demo1 /> */}
-      {/* <Demo2 /> */}
-      <Demo3 />
+      <Demo2 />
+      {/* <Demo3 /> */}
+      
     </div>
   );
 }
